53daca1e1453e4f3b31546eeab0ca40659788458
NoteYun.COM <support@noteyun.com>
2013-11-03 19:01:53 +0800

    No comment defined

--- application/modules/admin/controllers/IndexController.php
+++ application/modules/admin/controllers/IndexController.php
@@ -9,6 +9,18 @@ class Admin_IndexController extends Admin_BasicController
         $username = Domain_Util_General::getLoginAdministrator();
 
         try {
+            $search = new Domain_Search_Contents();
+            $search->setPublish(false);
+            $domainServiceContents = new Domain_Service_Contents();
+            $total = $domainServiceContents->getContentsNums($search);
+            $this->view->unpublicContentsNums = $total;
+            
+            $search = new Domain_Search_Comment();
+            $search->setPublish(false);
+            $domainServiceComment = new Domain_Service_Comment();
+            $total = $domainServiceComment->getCommentNums($search);
+            $this->view->unpublicCommentNums = $total;
+            
             $domainServicePermission = new Domain_Service_Permission();
             $adminAccreditList = $domainServicePermission->getAdminAccreditList($username);
             if ($adminAccreditList == null) {
@@ -32,5 +44,52 @@ class Admin_IndexController extends Admin_BasicController
 
     public function adminAction()
     {
+        $domainServiceConfiguration = new Domain_Service_Configuration();
+        
+        $this->view->websiteHtmlTitle = $domainServiceConfiguration->getConfigurationView(
+            "website_html_title");
+        $this->view->websiteCloseSign = $domainServiceConfiguration->getConfigurationView(
+            "website_close_sign");
+        $this->view->websiteVisitAnalytics = $domainServiceConfiguration->getConfigurationView(
+            "website_visit_analytics");
+        $this->view->attachmentHostName = $domainServiceConfiguration->getConfigurationView(
+            "attachment_hostname");
+        $this->view->attachmentRootPath = $domainServiceConfiguration->getConfigurationView(
+            "attachment_rootpath");
+        $this->view->websiteMetaKeywords = $domainServiceConfiguration->getConfigurationView(
+            "website_meta_keywords");
+        $this->view->websiteMetaDescript = $domainServiceConfiguration->getConfigurationView(
+            "website_meta_descript");
+        
+        $domainServiceCategory = new Domain_Service_Category();
+        $total = $domainServiceCategory->getCategoryNums(
+            new Domain_Search_Category());
+        $this->view->categoryNums = $total;
+        
+        $domainServiceContents = new Domain_Service_Contents();
+        $total = $domainServiceContents->getContentsNums(
+                        new Domain_Search_Contents());
+        $this->view->contentsNums = $total;
+        
+        
+        $search = new Domain_Search_Contents();
+        $search->setPublish(false);
+        $total = $domainServiceContents->getContentsNums($search);
+        $this->view->unpublicContentsNums = $total;
+        
+        $domainServiceComment = new Domain_Service_Comment();
+        $total = $domainServiceComment->getCommentNums(
+            new Domain_Search_Comment());
+        $this->view->commentNums = $total;
+        
+        $search = new Domain_Search_Comment();
+        $search->setPublish(false);
+        $total = $domainServiceComment->getCommentNums($search);
+        $this->view->unpublicCommentNums = $total;
+        
+        $domainServiceAttachment = new Domain_Service_Attachment();
+        $total = $domainServiceAttachment->getAttachmentNums(
+            new Domain_Search_Attachment());
+        $this->view->attachmentNums = $total;
     }
 }
